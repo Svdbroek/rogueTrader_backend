3db044a56b99cd931c70e95188f87a1917f7be1c
Svdbroek <s_in_the_water@hotmail.com>
2020-01-08 11:18:09 +0100

    forgot to add index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const port = process.env.PORT || 4000;
 
 const bodyParser = require("body-parser");
 const loginRouter = require("./auth/router");
+const characterRouter = require("./character/router");
 
 const parserMiddleware = bodyParser.json();
 const corsMiddleware = cors();
@@ -12,5 +13,6 @@ const corsMiddleware = cors();
 app.use(corsMiddleware);
 app.use(parserMiddleware);
 app.use(loginRouter);
+app.use(characterRouter);
 
 app.listen(port, console.log(`listening on port ${port}`));
